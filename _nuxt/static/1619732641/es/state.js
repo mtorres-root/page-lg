3fe208fde6a369b70de1262c531168e9d711d032
mtorres-root <74983767+mtorres-root@users.noreply.github.com>
2021-04-29 16:45:40 -0500

    v0.0.1

---
+++ _nuxt/static/1619732641/es/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpage-lg\u002F_nuxt\u002Fstatic\u002F1619732641",layout:"default",error:a,state:{i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fes",config:{_app:{basePath:"\u002Fpage-lg\u002F",assetsPath:"\u002Fpage-lg\u002F_nuxt\u002F",cdnURL:a}}}}(null));
