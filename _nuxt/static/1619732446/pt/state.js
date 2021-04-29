d3b637d9de66d0e8f9b9fbe2c797bc5a45926dce
mtorres-root <74983767+mtorres-root@users.noreply.github.com>
2021-04-29 16:42:32 -0500

    V 0.0.1

---
+++ _nuxt/static/1619732446/pt/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1619732446",layout:"default",error:a,state:{i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fpt",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
